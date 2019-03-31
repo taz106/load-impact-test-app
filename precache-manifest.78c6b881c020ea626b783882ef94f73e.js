da5c1f7522b35df5823213c76972be8b2ccad3cd
taz106 <tazdik.ershad@gmail.com>
2019-04-01 01:59:30 +0200

    Updates

---
+++ precache-manifest.78c6b881c020ea626b783882ef94f73e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "867a980ca5ce04982d30",
+    "url": "/load-impact-test-app/static/js/runtime~main.6f0e5905.js"
+  },
+  {
+    "revision": "f96a78ee75aca91e2f68",
+    "url": "/load-impact-test-app/static/js/main.975c268e.chunk.js"
+  },
+  {
+    "revision": "9341444e68a92f1c715f",
+    "url": "/load-impact-test-app/static/js/2.73548b75.chunk.js"
+  },
+  {
+    "revision": "f96a78ee75aca91e2f68",
+    "url": "/load-impact-test-app/static/css/main.0f69becc.chunk.css"
+  },
+  {
+    "revision": "9341444e68a92f1c715f",
+    "url": "/load-impact-test-app/static/css/2.b54f0897.chunk.css"
+  },
+  {
+    "revision": "b260d04fcb7ae24cc417539187a8f955",
+    "url": "/load-impact-test-app/index.html"
+  }
+];
